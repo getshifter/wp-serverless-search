de189b4ba180b35074fe4e765f99559e0ade768a
Daniel Olson <emaildano@gmail.com>
2018-10-15 18:54:20 -0400

    first commit based on example: toddc8787/pen/Rorzeq

---
+++ wp-serverless-search.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: WP Serverless Search
+Author: Daniel Olson
+Author URI: https://github.com/emaildano/wp-serverless-search
+Description: Serverless WordPress Search
+*/
+
+/*
+ * Scripts
+ */
+add_action('wp_enqueue_scripts', 'wp_sls_search_js' );
+add_action('admin_enqueue_scripts', 'wp_sls_search_js' );
+
+function wp_sls_search_js() {
+  
+  $shifter_js = plugins_url( 'main/main.js', __FILE__ );
+  
+  wp_register_script('wp-sls-search-js', $shifter_js, array( 'jquery' ));
+  wp_localize_script('wp-sls-search-js', 'ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+
+  wp_register_script( 'fusejs', 'https://cdnjs.cloudflare.com/ajax/libs/fuse.js/3.2.1/fuse.min.js', null, null, false );
+  wp_localize_script('fusejs', 'ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+
+  wp_register_script( 'fiber', 'https://cdnjs.cloudflare.com/ajax/libs/fiber/1.0.5/fiber.min.js', null, null, false );
+  wp_localize_script('fiber', 'ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+
+  wp_register_script( 'lodash', 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.14.1/lodash.min.js', null, null, false );
+  wp_localize_script('lodash', 'ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+
+  wp_enqueue_script('wp-sls-search-js');
+  wp_enqueue_script('fiber');
+  wp_enqueue_script('lodash');
+  wp_enqueue_script('fusejs');
+
+}
