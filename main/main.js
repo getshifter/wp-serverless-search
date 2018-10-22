510ced452700002dd533029997ee5e2fe7216b3e
Daniel Olson <emaildano@gmail.com>
2018-10-22 18:47:12 -0400

    updated URL js

--- main/main.js
+++ main/main.js
@@ -1,23 +1,7 @@
-function RemoveBaseUrl(url) {
-  /*
-   * Replace base URL in given string, if it exists, and return the result.
-   *
-   * e.g. "http://localhost:8000/api/v1/blah/" becomes "/api/v1/blah/"
-   *      "/api/v1/blah/" stays "/api/v1/blah/"
-   */
-  var baseUrlPattern = /^https?:\/\/[a-z\:0-9.]+/;
-  var result = "";
-
-  var match = baseUrlPattern.exec(url);
-  if (match != null) {
-      result = match[0];
-  }
-
-  if (result.length > 0) {
-      url = url.replace(result, "");
-  }
-
-  return url;
+function postUrl(url) {
+  var parser = document.createElement('a');
+  parser.href = url;
+  return parser.pathname;
 }
 
 jQuery(window).ready(function ($) {
@@ -78,7 +62,7 @@ jQuery(window).ready(function ($) {
         searchArray.push({
           "title": el.find('title').text(),
           "description": el.find('description').text(),
-          "link": RemoveBaseUrl(el.find('link').text())
+          "link": postUrl(el.find('link').text())
         });
 
       });
